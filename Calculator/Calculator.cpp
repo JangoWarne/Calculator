b92e19d3b59ec5827d8e3d54d842c20cf288c961
JangoWarne <joshuamarkwalker@gmail.com>
2017-02-26 14:56:21 +0000

    Initial basic calculator
    Created during first session

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,134 @@
+// Calculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+// functions
+float add_single(int num1, int num2);
+float subtract_single(int num1, int num2);
+float multiply_single(int num1, int num2);
+float divide_single(int num1, int num2);
+
+
+// Main
+int main()
+{
+	bool continue(true);
+
+	while (continue == false)
+	{
+		// take user input
+		char input_string[3];
+		std::cout << "Enter calculation:  ";
+		std::cin >> input_string;
+
+
+		// split into three parts
+		int num1;
+		char operand;
+		int num2;
+
+		num1 = atoi(&input_string[0]);
+		operand = input_string[1];
+		num2 = atoi(&input_string[2]);
+
+		std::cout << input_string[0] << std::endl;
+		std::cout << input_string[1] << std::endl;
+		std::cout << input_string[2] << std::endl;
+
+		std::cout << num1 << std::endl;
+		std::cout << operand << std::endl;
+		std::cout << num2 << std::endl;
+
+		// calculate result
+		float result;
+
+		if (operand == '+')
+		{
+			// add
+			result = add_single(num1, num2);
+		}
+		else if (operand == '-')
+		{
+			// subtract
+			result = subtract_single(num1, num2);
+		}
+		else if ((operand == '*') || (operand == 'x'))
+		{
+			// multiply
+			result = multiply_single(num1, num2);
+		}
+		else if (operand == '/')
+		{
+			// divide
+			result = divide_single(num1, num2);
+		}
+		else
+		{
+			// do nothing
+		}
+
+
+
+		// output result
+
+		std::cout << "Answer is:  " << result << std::endl << std::endl;
+
+
+
+		// check if to continue
+		char continue_str;
+		std::cout << "Continue (Y/N):  ";
+		std::cin >> continue_str;
+	}
+	
+
+	system("PAUSE");
+
+    return 0;
+}
+
+
+
+// add two numbers
+float add_single(int num1, int num2)
+{
+	float result;
+
+	result = num1 + num2;
+
+	return result;
+}
+
+// subtract two numbers
+float subtract_single(int num1, int num2)
+{
+	float result;
+
+	result = num1 - num2;
+
+	return result;
+}
+
+// multiply two numbers
+float multiply_single(int num1, int num2)
+{
+	float result;
+
+	result = num1 * num2;
+
+	return result;
+}
+
+// divide two numbers
+float divide_single(int num1, int num2)
+{
+	float result;
+	float float1 = num1;
+	float float2 = num2;
+
+	result = float1 / float2;
+
+	return result;
+}
